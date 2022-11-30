51760a0e220f882ab13318f5f0b24c6b61409a1f
Richard-Harris1992 <104505323+Richard-Harris1992@users.noreply.github.com>
2022-11-30 10:49:55 -0600

    first express assignment

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const students = require('./studentInfo');
+const port = 3000;
+const app = express();
+
+const fs = require('fs') 
+app.engine('madeline', (filePath, options, callback) => { 
+  fs.readFile(filePath, (err, content) => {
+    if (err) return callback(err)
+    
+    const rendered = content.toString()
+      .replace('#name#', '<h1>' + options.name + '</h1>')
+      .replace('#id#', '<h2>' + options.id + '</h2>')
+      .replace('#school#','<h2>'+ options.school + '</h2>')
+      .replace('#message#', '<h1>' + options.message + '</h1>')
+    return callback(null, rendered);
+  })
+})
+app.set('views', './views') ;
+app.set('view engine', 'madeline');
+
+
+function student() {
+     let result ='';
+     result += (`<ul><h2>Students</h2>`);
+
+    for(let i = 0; i < students.length; i++) {
+        result += (`<li><a href="./students/${i}">${students[i].name}</a></li>\n`);
+    }
+    result += (`</ul>`);
+ 
+    return result;
+}
+
+app.get('/', (req, res) => {
+    res.send (
+        `<h1>Welcome to our homepage</h1>
+        <h2><a href="./about"> About </a></h2>
+        <h2><a href="./students">Students</a></h2>
+        <h2><a href="./contact">Contact Info</a></h2>`
+    );
+});
+
+app.get('/about/', (req, res) =>{
+    res.send(`<h2> Welcome to Harris University, where all of you dreams will come true, tuition is 20000 a semester and we guarantee nothing.</br>Pay us please. </h2>`);
+});
+
+app.get('/students', (req, res) => {
+    res.send(student());
+});
+
+app.get('/students/:idOfStudent', (req, res) =>{
+    res.render('template', { name: students[req.params.idOfStudent].name, id: students[req.params.idOfStudent].id, school: students[req.params.idOfStudent].school});
+});
+
+app.get('/contact', (req, res) => {
+    res.send(`<h2> Phone number is unavailable, email as well. You can send us a check though. <a href="/contact/payMe">Click here to send money though</a></h2>`);
+});
+
+app.get('/contact/payMe', (req, res) => {
+    res.render('money', {message: `Routing number: 111111111111 </br> PayPal: PayMe123 </br> Crypto Wallet (prefered for umm..security): 122e433s3454s-AWkSSidd23
+    </br> Your money... I mean education is important to us. Please join us.`});
+});
+
+
+
+app.listen(port, () => console.log(`3000 is used`));
---
+++ studentInfo.js
@@ -0,0 +1,40 @@
+studentInfo = [
+    {
+        name: 'Richard',
+        id: 1,
+        school: 'HarrisUniv'
+    },
+    {
+        name: 'Denisse',
+        id: 2,
+        school: 'HarrisUniv'
+    },
+    {
+        name: 'Judah',
+        id: 3,
+        school: 'HarrisUniv'
+    },
+    {
+        name: 'Makayla',
+        id: 4,
+        school: 'HarrisUniv'
+    },
+    {
+        name: 'Lee',
+        id: 5,
+        school: 'HarrisUniv'
+    },
+    {
+        name: 'George',
+        id: 6,
+        school: 'HarrisUniv'
+    },
+    {
+        name: 'Cristian',
+        id: 7,
+        school: 'HarrisUniv'
+    }
+
+]
+
+module.exports = studentInfo
